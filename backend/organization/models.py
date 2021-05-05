dbf4cc713b77080001618ab90c1e6db5ea60262a
Dhananjhay <dhananjhay03@gmail.com>
2021-05-05 02:12:51 -0600

    Changes to front page

--- backend/organization/models.py
+++ backend/organization/models.py
@@ -2,11 +2,11 @@ from django.db import models
 
 
 class Organization(models.Model):
-    name = models.CharField(max_length=500)
-    email = models.CharField(max_length=500)
-    address = models.CharField(max_length=1000)
-    phone = models.Charfield(max_length=50)
-    logo = models.CharField(max_length=50)
+    name = models.CharField(max_length=500, default=None)
+    email = models.CharField(max_length=500, default=None)
+    address = models.CharField(max_length=1000, default=None)
+    phone = models.CharField(max_length=50, default=None)
+    logo = models.CharField(max_length=50, default=None)
 
     def __str__(self):
         return "{}".format(self.name)
