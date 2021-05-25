83add6e877c24e43fcc00a7d3ea30af2f1a16959
ayaachi <ayaachi712@gmail.com>
2021-05-25 13:36:42 +0530

    changed drawer bg / added top btn / made table scrollable y

--- backend/covid_site/settings.py
+++ backend/covid_site/settings.py
@@ -110,8 +110,8 @@ DATABASES = {
     'default': {
         'ENGINE': "django.db.backends.mysql",
         'NAME': "covid-site",
-        'USER': "ayaachi",
-        'PASSWORD': "Chamber007",
+        'USER': "",
+        'PASSWORD': "",
         'PORT': 3306,
         'HOST': '127.0.0.1',
     }
